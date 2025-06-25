537fc609d94ae4402f58a625554f77a910fbe1cc
ningen <ningen361@gmail.com>
2025-06-26 02:06:36 +0900

    Initial commit from create-remix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A dev shell for the todo app";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
+      in
+      {
+        devShell = pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+          ];
+        };
+      });
+}
